b3b93f6519d82fc6761984dd99cfcf8e99b18f91
yashwanthbalaram <yashwanth.baratam@zeptonow.com>
2025-04-26 10:00:29 +0530

    add controllers for Answer, Comment, and Question with basic CRUD endpoints

---
+++ src/main/java/com/example/quoraclone/controllers/CommentController.java
@@ -0,0 +1,10 @@
+package com.example.quoraclone.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+public class CommentController {
+
+}
